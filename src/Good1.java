445966785826ab2a2b3430ae2b128e9c7d51e7da
Pojar60 <1pojarskij@gmail.com>
2023-06-10 12:38:06 +0300

    Commit for command for reset1

--- src/Good1.java
+++ src/Good1.java
@@ -1,5 +1,5 @@
 public class Good1 {
     public static void main(String[] args) {
-        System.out.println("Commit for command revert");
+        System.out.println("Commit for command Reset1");
     }
 }
