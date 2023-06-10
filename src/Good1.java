01a9efb82a3df535866a7627199e686082ecdbe0
Pojar60 <1pojarskij@gmail.com>
2023-06-10 12:38:21 +0300

    Commit for command for reset2

--- src/Good1.java
+++ src/Good1.java
@@ -1,5 +1,5 @@
 public class Good1 {
     public static void main(String[] args) {
-        System.out.println("Commit for command Reset1");
+        System.out.println("Commit for command Reset2");
     }
 }
